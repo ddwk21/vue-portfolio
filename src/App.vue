506a2aa24883d46dbd4d9dbc023a781e5825942b
Mitchell Lemieux <ddwk21@gmail.com>
2023-09-18 19:29:56 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="sidebar">
+      Nav here
+    </div>
+    <div class="summary">
+      <div class="summaryContent">
+        <h1>MITCHELL LEMIEUX</h1>
+        <p>-engineer-designer-developer-</p>
+      </div>
+    </div>
+    <div class="hero">
+      Hero Here
+    </div>
+
+    <div class="circle" id="Circle1">Circle</div>
+    <div id="Circle2"></div>
+  </main>
+</template>
+
+<style scoped>
+
+
+
+
+</style>
